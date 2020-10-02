c477611230c7eec50013ceeb2c475da81c6a5bca
as ahmed <rakibasrr13@gmail.com>
2020-10-02 18:41:42 +0300

    burj-al-arab'

--- src/components/Bookings/Bookings.js
+++ src/components/Bookings/Bookings.js
@@ -1,34 +1,34 @@
- import React, { useContext, useEffect, useState } from 'react';
- import { UserContext } from '../../App';
- 
- const Bookings = () => {
-     const [bookings,setBookings] = useState([]);
-     const [loggedInUser, setLoggedInUser] = useContext(UserContext);
+import React, { useContext, useEffect, useState } from 'react';
+import { UserContext } from '../../App';
 
-     useEffect(() => {
-         fetch('http://localhost:4000/bookings?email='+loggedInUser.email,{
-          //   001
-             method:'GET',
-             headers: { 
-                 'Content-type': 'application/json',
-                 authorization: `Bearer ${sessionStorage.getItem('token')}`
- //user 001 login ^^ korar por tar info idToken ze info gula chilo oi info gula amra sessionStorage theke tar idToken niye nichi 
- //er por ze info gula paichi oi info gulo backed e pathai dichi..    
-                }
-         })
-         .then(res => res.json())
-         .then(data => setBookings(data));
-     },[])
+const Bookings = () => {
+    const [bookings, setBookings] = useState([]);
+    const [loggedInUser, setLoggedInUser] = useContext(UserContext);
 
-     return (
-         <div>
-             <h3>you have: {bookings.length} bookings</h3>
-             {
-                 bookings.map(book => <li key={book._id}> {book.name} from:{(new Date(book.checkIn).toDateString('dd/MM/yyy'))} to: {(new Date (book.checkOut).toDateString('dd/MM/yyy'))}</li>)
-             //ei code diye tumi data take sting theke( normal ^^^ date e convert korte parba   ^^      ) --                    
-             }
-         </div>
-     );
- };
- 
- export default Bookings;
+    useEffect(() => {
+        fetch('http://localhost:4000/bookings?email=' + loggedInUser.email, {
+            //   001
+            method: 'GET',
+            headers: {
+                'Content-type': 'application/json',
+                authorization: `Bearer ${sessionStorage.getItem('token')}`
+                //user 001 login ^^ korar por tar info idToken ze info gula chilo oi info gula amra sessionStorage theke tar idToken niye nichi 
+                //er por ze info gula paichi oi info gulo backed e pathai dichi..    
+            }
+        })
+            .then(res => res.json())
+            .then(data => setBookings(data));
+    }, [])
+
+    return (
+        <div>
+            <h3>you have: {bookings.length} bookings</h3>
+            {
+                bookings.map(book => <li key={book._id}> {book.name} from:{(new Date(book.checkIn).toDateString('dd/MM/yyy'))} to: {(new Date(book.checkOut).toDateString('dd/MM/yyy'))}</li>)
+                //ei code diye tumi data take sting theke( normal ^^^ date e convert korte parba   ^^      ) --                    
+            }
+        </div>
+    );
+};
+
+export default Bookings;
